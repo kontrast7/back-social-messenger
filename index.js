ee51d5f4ec717525230239920d1874af3e4f89a5
kontrast7 <maks.mashko@gmail.com>
2022-03-17 19:55:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use(cors())
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
