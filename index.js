6eca37cadb1dcd8186230a933a086b9a622af0b3
kontrast7 <maks.mashko@gmail.com>
2022-03-23 16:13:43 +0300

    fix

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const router = express.Router();
 const path = require("path");
 const cors = require("cors");
 const bodyParser  = require("body-parser")
+const http  = require("http");
+const socketIo = require("socket.io");
 
 dotenv.config();
 
@@ -26,21 +28,36 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+// app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
-app.use(express.json());
+// app.use(express.json());
 app.use(bodyParser.json({limit: "7mb"}));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({limit: "7mb", extended: false}));
-app.use(helmet());
-app.use(morgan("common"));
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-  })
-);
+// app.use(helmet());
+// app.use(morgan("common"));
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+
+const corsOptions = {
+  credentials: true,
+  origin: (origin, callback ) => {
+      // if(whitelist.includes(origin || ""))
+      //     return callback(null, true)
+      //
+      // callback(new Error('Not allowed by CORS'));
+      // console.log("origin: ", origin);
+      callback(null, true); // everyone is allowed
+  }
+};
+
+app.use(cors(corsOptions));
+
+
 
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -50,23 +67,23 @@ app.use(
 //   next();
 // });
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "public/images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, req.body.name);
+//   },
+// });
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  try {
-    return res.status(200).json("File uploded successfully");
-  } catch (error) {
-    console.error(error);
-  }
-});
+// const upload = multer({ storage: storage });
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   try {
+//     return res.status(200).json("File uploded successfully");
+//   } catch (error) {
+//     // console.error(error);
+//   }
+// });
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -76,56 +93,4 @@ app.use("/api/messages", messageRoute);
 
 app.listen(PORT, () => {
   console.log("Backend server is running!");
-});
-
-
-const io = require("socket.io")(8900, {
-  cors: {
-    origin: "*",
-  },
-});
-
-let users = [];
-
-const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId });
-};
-
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
-
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
-};
-
-io.on("connection", (socket) => {
-  //when ceonnect
-  console.log("a user connected.");
-
-
-  
-
-  //take userId and socketId from user
-  socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
-  });
-
-  //send and get message
-  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    const user = getUser(receiverId);
-    io.to(user.socketId).emit("getMessage", {
-      senderId,
-      text,
-    });
-  });
-
-  //when disconnect
-  socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-    removeUser(socket.id);
-    io.emit("getUsers", users);
-  });
 });
