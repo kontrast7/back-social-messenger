71d3acd806967b3e8426d2d20cc09703664881ac
kontrast7 <maks.mashko@gmail.com>
2022-03-23 19:35:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,59 +31,59 @@ mongoose.connect(
 // app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
-// app.use(express.json());
+app.use(express.json());
 app.use(bodyParser.json({limit: "7mb"}));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({limit: "7mb", extended: false}));
-// app.use(helmet());
-// app.use(morgan("common"));
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
+app.use(helmet());
+app.use(morgan("common"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-const corsOptions = {
-  credentials: true,
-  origin: (origin, callback ) => {
-      // if(whitelist.includes(origin || ""))
-      //     return callback(null, true)
-      //
-      // callback(new Error('Not allowed by CORS'));
-      // console.log("origin: ", origin);
-      callback(null, true); // everyone is allowed
-  }
-};
-
-app.use(cors(corsOptions));
-
-
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next();
-// });
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "public/images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, req.body.name);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-// app.post("/api/upload", upload.single("file"), (req, res) => {
-//   try {
-//     return res.status(200).json("File uploded successfully");
-//   } catch (error) {
-//     // console.error(error);
+// const corsOptions = {
+//   credentials: true,
+//   origin: (origin, callback ) => {
+//       // if(whitelist.includes(origin || ""))
+//       //     return callback(null, true)
+//       //
+//       // callback(new Error('Not allowed by CORS'));
+//       // console.log("origin: ", origin);
+//       callback(null, true); // everyone is allowed
 //   }
-// });
+// };
+
+// app.use(cors(corsOptions));
+
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
